cc43d2d9a750783b22e502506109f9e0e4a50d56
Sreeram Panigrahi <panigrahi.sreeram@gmail.com>
2025-05-13 01:16:16 +0530

    create portfolio page

---
+++ src/content_option.js
@@ -0,0 +1,167 @@
+const logotext = "SREERAM";
+const meta = {
+    title: "Sreeram Panigrahi",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Sreeram Panigrahi",
+    animated: {
+        first: "I love Technology",
+        second: "I develop fullstack",
+        third: "Vibe Coding lately",
+    },
+    description: "SDE 2 @ Goldman Sachs | Ex-Amazon, Swiggy",
+    description2: "Fullstack Developer based in Hyderabad, India",
+    your_img_url: "https://iili.io/38SFjiF.jpg",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "I am a Backend developer who excels in AWS services, Java and Springboot. I have led significant projects in Catalog Systems, notably implementing migration from Kafka to AWS SQS, reducing API latency, and designing database migration plans from SQL to NoSQL.",
+    aboutme2: "In my free time I also delve into GoLang, System Design, Scaling services and open source tools.",
+};
+const worktimeline = [{
+        jobtitle: "SDE 2 (Associate)",
+        where: "Goldman Sachs",
+        date: "2025 - Present",
+    },
+    {
+        jobtitle: "SDE 1",
+        where: "Swiggy",
+        date: "2022-2024",
+    },
+    {
+        jobtitle: "SDE Intern",
+        where: "Amazon",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "Java, Spring, Spring Boot",
+        value: 90,
+    },
+    {
+        name: "AWS Services",
+        value: 85,
+    },
+    {
+        name: "Python",
+        value: 80,
+    },
+    {
+        name: "Typescript/React + HTML/CSS/Bootstrap",
+        value: 60,
+    },
+    {
+        name: "Curiosity and Learning",
+        value: 100,
+    },
+];
+
+const services = [{
+        title: "Cloud Migrations",
+        description: "SQL to NoSQL, Kafka to SQS, etc.",
+    },
+    {
+        title: "Technology Upgrade",
+        description: "Java to gRPC modernization and upgrades.",
+    },
+    {
+        title: "AWS Integration",
+        description: "DynamoDB, SQS, Lambda, EC2, Elasticache, X-ray, CloudWatch monitoring.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "panigrahi.sreeram@gmail.com",
+    YOUR_FONE: "(+91)7978426120",
+    description: "I am available for freelance or full-time positions. Contact me and let's talk.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_9gm1wkk",
+    YOUR_TEMPLATE_ID: "template_s20pb6y",
+    YOUR_USER_ID: "A1Zc_SocQa9ABBzAJ",
+};
+
+const socialprofils = {
+    github: "https://github.com/theSreeRam",
+    facebook: "https://www.facebook.com/panigrahi.sree/",
+    linkedin: "https://www.linkedin.com/in/sreeram-panigrahi/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
